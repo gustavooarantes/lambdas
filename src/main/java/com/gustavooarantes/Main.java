8edc3887c0c52bf68d8e333e68e291c0163f1fb5
gustavooarantes <profissional.gustavoa@gmail.com>
2025-09-21 09:13:57 -0300

    started describing and implementing lambdas (expression vs statement)

---
+++ src/main/java/com/gustavooarantes/Main.java
@@ -0,0 +1,54 @@
+package com.gustavooarantes;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+  @FunctionalInterface
+  interface MathOperation {
+    int operate(int a, int b);
+  }
+
+  public static void main(String[] args) {
+    /**
+     * Expression Lambda: (parameters-list) -> expression;
+     *
+     * <p>Statement Lambda: (parameters-list) -> {statements;}
+     */
+    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    int sumOfSquaredEvends =
+        numbers.stream()
+            .filter(number -> number % 2 == 0)
+            .mapToInt(number -> number * number)
+            .sum();
+
+    System.out.println(sumOfSquaredEvends);
+
+    /**
+     * Lambdas are like compact representations of an anonymous function (functions that lacks a
+     * specific name) that can be easily passed around. It shares common characteristics with
+     * methods, such as having parameters, a body, a return type, and the potential for a list of qa
+     * exceptions, although it isn't tied to a particular class. Lambdas can be passed as an
+     * argument to a method, or even be stored in a variable.
+     */
+  }
+
+  // Example of simplifying  a method using lambdas:
+  // Method (default)
+  public int add(int num1, int num2) {
+    int result = num1 + num2;
+    return result;
+  }
+
+  // Statement lambda
+  static MathOperation addOperation =
+      (num1, num2) -> {
+        int result = num1 + num2;
+        return result;
+      };
+
+  // Expression lambda
+  static MathOperation addOperation_ = (a, b) -> a + b;
+}
